029fb89d387242f5148a478ab28ddca10fbf900c
stephie-spec <compwork254@gmail.com>
2026-01-12 09:19:08 +0300

    adds seed file

--- models.py
+++ models.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 
+metadata = MetaData()
 db = SQLAlchemy(metadata=metadata)
 
 class Hero(db.Model):
